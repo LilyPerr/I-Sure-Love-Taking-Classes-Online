56d4ca893d301717fd4ea186a500f98a2c5f52b4
Lily Perry <lilyperr@usc.edu>
2020-09-05 21:31:50 -0400

    Javascript to generate a funny sentence

---
+++ words.js
@@ -0,0 +1,28 @@
+//returns a random number between min (included) and max (excluded), from w3schools
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+//returns a random funny string to be the first half of your sentence
+function getFunnyString(){
+  var words = ["When you've just rolled out of bed",
+  "When you've finally finished your first cup of coffee",
+  "When you're not wearing pjs anymore",
+  "When your microphone FINALLY works",
+  "When your internet isn't choppy",
+  "When you have clothes on",
+  "When you're sure that your camera is off",
+  "When you finished gathering all of your snacks",
+  "When you made sure to turn off your mic",
+  "When your glass of water is half full",
+  "When you have no background noise"];
+  var num = getRndInteger(0, words.length);
+  var funny = words[num];
+  return funny;
+ }
+ 
+ 
+ //returns the full sentence to display
+ function getFullSentence(){
+  return getFunnyString() + ", click Open Zoom Meetings.";
+ }
