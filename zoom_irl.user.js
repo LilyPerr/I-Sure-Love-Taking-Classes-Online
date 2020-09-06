4a484b741af13ee6683a77c9da465768a0e3e3ef
Swapnil Patel <sp70215@gmail.com>
2020-09-05 20:39:42 -0700

    Add retry to userscript in case of error
    Zoom loads the page via JS so if we're too early, the value we change is null. If it's null, the script will now wait a second and retry.

--- zoom_irl.user.js
+++ zoom_irl.user.js
@@ -7,32 +7,40 @@
 // @include     *.zoom.us/j/*
 // @updateURL   https://raw.githubusercontent.com/LilyPerr/lilyperr.github.io/master/zoom_irl.js
 // @downloadURL https://raw.githubusercontent.com/LilyPerr/lilyperr.github.io/master/zoom_irl.js
+// @run-at document-idle
 // ==/UserScript==
 
 (function() {
-    'use strict';
+'use strict';
 
-    //returns a random funny string to be the first half of your sentence
-    function getFunnyString(){
-        var words = ["When you've just rolled out of bed",
-                     "When you've finally finished your first cup of coffee",
-                     "When you're not wearing pjs anymore",
-                     "When your microphone FINALLY works",
-                     "When your internet isn't choppy",
-                     "When you have clothes on",
-                     "When you're sure that your camera is off",
-                     "When you finished gathering all of your snacks",
-                     "When you made sure to turn off your mic",
-                     "When your glass of water is half full",
-                     "When you have no background noise"];
-        return words[Math.floor(Math.random() * words.length)] + ", click Open Zoom Meetings.";
-    }
+//returns a random funny string to be the first half of your sentence
+function getFunnyString(){
+    var words = ["When you've just rolled out of bed",
+                 "When you've finally finished your first cup of coffee",
+                 "When you're not wearing pjs anymore",
+                 "When your microphone FINALLY works",
+                 "When your internet isn't choppy",
+                 "When you have clothes on",
+                 "When you're sure that your camera is off",
+                 "When you finished gathering all of your snacks",
+                 "When you made sure to turn off your mic",
+                 "When your glass of water is half full",
+                 "When you have no background noise"];
+    return words[Math.floor(Math.random() * words.length)] + ", click Open Zoom Meetings.";
+}
 
-    console.log("before loop");
-    while (document.querySelector("div > h1").innerHTML == null) {
-        console.log("in loop");
-    }
-    console.log("after loop");
+
+try {
     document.querySelector("div > h1").innerHTML = getFunnyString();
     console.log(getFunnyString());
+} catch (e) {
+    console.error(e.name + ': ' + e.message);
+    console.log("Retrying in 1 seconds");
+    setTimeout(
+        function() {
+            console.log("Retrying!");
+            document.querySelector("div > h1").innerHTML = getFunnyString();
+            console.log(getFunnyString());
+        }, 1000);
+}
 })();
